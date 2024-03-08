f2e6cf83339c2116a97fcaa70589a93463c5f523
emrgnt-cmplxty <owen@algofi.org>
2024-03-08 15:52:31 -0800

    fix config.json path

--- src/app.py
+++ src/app.py
@@ -3,4 +3,4 @@ from r2r.main import E2EPipelineFactory
 # Creates a pipeline with default configuration
 # This is the main entry point for the application
 # The pipeline is built using the `config.json` file
-app = E2EPipelineFactory.create_pipeline()
+app = E2EPipelineFactory.create_pipeline(config_path='config.json')
