3d1e9175fba78c4719cb88d5575db5e52f8c050f
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-03-14 22:45:48 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,4 +3,4 @@ from r2r.main import E2EPipelineFactory, R2RConfig
 # Creates a pipeline with default configuration
 # This is the main entry point for the application
 # The pipeline is built using the `config.json` file
-app = E2EPipelineFactory.create_pipeline(config=R2RConfig.load_config())
+app = E2EPipelineFactory.create_pipeline(config=R2RConfig.load_config("config.json"))
