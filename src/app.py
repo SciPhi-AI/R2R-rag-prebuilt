849215c7538402a28e844ae02934f95cbd8f7c9f
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-06-03 09:35:03 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,26 +1,54 @@
 import os
-from r2r.main import E2EPipelineFactory, R2RConfig
+import logging
+from enum import Enum
+from r2r import (
+    R2RConfig,
+    R2RAppBuilder,
+    # For Web Search
+    R2RWebSearchPipe,
+    SerperClient,
+    # For HyDE & the like.
+    R2RPipeFactoryWithMultiSearch
+)
 
-# Loads the local configuration file
-config = R2RConfig.load_config("config.json")
+logger = logging.getLogger(__name__)
 
-env_setting = os.getenv("RAG_PIPELINE", None)
+current_file_path = os.path.dirname(__file__)
+configs_path = os.path.join(current_file_path, "..", "..", "..")
 
-# Selects the pipeline to use
-rag_pipeline = env_setting or config.app.get("rag_pipeline", "qna")
-if rag_pipeline == "qna":
-    from r2r.pipelines import QnARAGPipeline
-    rag_pipeline_impl = QnARAGPipeline
-elif rag_pipeline == "web":
-    from r2r.pipelines import WebRAGPipeline
-    rag_pipeline_impl = WebRAGPipeline
-elif rag_pipeline == "agent":
-    from r2r.pipelines import AgentRAGPipeline
-    rag_pipeline_impl = AgentRAGPipeline
-elif rag_pipeline == "hyde":
-    from r2r.pipelines import HyDEPipeline
-    rag_pipeline_impl = HyDEPipeline
-else:
-    raise ValueError(f"Invalid pipeline: {rag_pipeline}")
+CONFIG_OPTIONS = {
+    "default": None,
+    "local_ollama": os.path.join(configs_path, "local_ollama.json"),
+}
 
-app = E2EPipelineFactory.create_pipeline(config=config, rag_pipeline_impl=rag_pipeline_impl)
+class RagPipeline(Enum):
+    QNA = "qna"
+    WEB = "web"
+    HYDE = "hyde"
+
+def r2r_app(config_name: str = "default", rag_pipeline: RagPipeline = RagPipeline.QNA):
+    if config_path := CONFIG_OPTIONS.get(config_name):
+        logger.info(f"Using config path: {config_path}")
+        config = R2RConfig.from_json(config_path)
+    else:
+        default_config_path = os.path.join(configs_path, "config.json")
+        logger.info(f"Using default config path: {default_config_path}")
+        config = R2RConfig.from_json(default_config_path)
+
+    if config.embedding.provider == 'openai' and 'OPENAI_API_KEY' not in os.environ:
+        raise ValueError("Must set OPENAI_API_KEY in order to initialize OpenAIEmbeddingProvider.")
+
+    if rag_pipeline == RagPipeline.QNA:
+        return R2RAppBuilder(config).build()
+    elif rag_pipeline == RagPipeline.WEB:
+        # Create search pipe override and pipes
+        web_search_pipe = R2RWebSearchPipe(
+            serper_client=SerperClient()  # TODO - Develop a `WebSearchProvider` for configurability
+        )
+        return R2RAppBuilder(config).with_search_pipe(web_search_pipe).build()
+    elif rag_pipeline == RagPipeline.HYDE:
+        return R2RAppBuilder(config).with_pipe_factory(R2RPipeFactoryWithMultiSearch) \
+            .build(
+                # Add optional override arguments which propagate to the pipe factory
+                task_prompt_name="hyde",
+            )
