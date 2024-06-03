055917469db9a50c290d19bd67f97d7af54bc27e
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-06-03 09:43:39 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -14,29 +14,14 @@ from r2r import (
 logger = logging.getLogger(__name__)
 
 current_file_path = os.path.dirname(__file__)
-configs_path = os.path.join(current_file_path, "..", "..", "..")
-
-CONFIG_OPTIONS = {
-    "default": None,
-    "local_ollama": os.path.join(configs_path, "local_ollama.json"),
-}
 
 class RagPipeline(Enum):
     QNA = "qna"
     WEB = "web"
     HYDE = "hyde"
 
-def r2r_app(config_name: str = "default", rag_pipeline: RagPipeline = RagPipeline.QNA):
-    if config_path := CONFIG_OPTIONS.get(config_name):
-        logger.info(f"Using config path: {config_path}")
-        config = R2RConfig.from_json(config_path)
-    else:
-        default_config_path = os.path.join(configs_path, "config.json")
-        logger.info(f"Using default config path: {default_config_path}")
-        config = R2RConfig.from_json(default_config_path)
-
-    if config.embedding.provider == 'openai' and 'OPENAI_API_KEY' not in os.environ:
-        raise ValueError("Must set OPENAI_API_KEY in order to initialize OpenAIEmbeddingProvider.")
+def r2r_app( rag_pipeline: RagPipeline = RagPipeline.QNA):
+    config = R2RConfig.from_json('config.json')
 
     if rag_pipeline == RagPipeline.QNA:
         return R2RAppBuilder(config).build()
