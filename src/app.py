f940d2bb728eabc7953d23f49412e4c59239e4a8
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2024-08-05 15:12:40 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,21 +3,9 @@ from enum import Enum
 from r2r import (
     R2RConfig,
     R2RBuilder,
-    # For Web Search
-    WebSearchPipe,
-    SerperClient,
-    # For HyDE & the like.
-    R2RPipeFactoryWithMultiSearch
 )
 
-
-class RagPipeline(Enum):
-    QNA = "qna"
-    WEB = "web"
-    HYDE = "hyde"
-
 def r2r_app():
-    
     config = R2RConfig.from_toml("r2r.toml")
     return R2RBuilder(config).build().app
 
